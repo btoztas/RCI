e62872b73500ced714d9be8952032051aa0c543c
Bruno Gonçalves7 <btoztas@gmail.com>
2016-03-15 20:26:42 +0000

    Eliminar ficheiros desnecessários

--- tcpserver.c
+++ tcpserver.c
@@ -10,61 +10,71 @@
 
 #define PORT 58000
 
+
+
 int main(){
 
 	int socketfd, newsocketfd;
-	struct hostent *hostptr;
-	struct sockaddr_in serveraddr, clientaddr;
-	int addrlen, clientlen;
-	char buffer[128];
-	/*struct hostent *h;
-	struct in_addr *a;
-	*/
+	struct sockaddr_in serveraddr;
+	int clientlen;
+	char buffer[128], msgRecieved[128];
+	int n;
+	
+	
+	
+	memset((void*)&(buffer),(int)'\0',sizeof((buffer)));
 	
 	socketfd=socket(AF_INET, SOCK_STREAM, 0);
 	if(socketfd==-1){
-		printf("Error opening socket");
-		exit(1);
+	  printf("Error opening socket\n");
+	  exit(1);
 	}
-	/*
-	if((h=gethostbyname("lima"))==NULL){
-		printf("Error on getting host");
-		exit(1);
-	}
-	
-	printf("official host name: %s\n", h->h_name);
-	a=(struct in_addr*)h->h_addr_list[0];*/
 
-	memset((void*)&serveraddr,(int)'\0',sizeof(serveraddr));
-	serveraddr.sin_family=AF_INET;
-	serveraddr.sin_addr.s_addr=htonl(INADDR_ANY);
-	serveraddr.sin_port=htons((u_short)PORT);
+	memset((void*)&(serveraddr),(int)'\0',sizeof((serveraddr)));
+	(serveraddr).sin_family=AF_INET;
+	(serveraddr).sin_addr.s_addr=htonl(INADDR_ANY);
+	(serveraddr).sin_port=htons((u_short)PORT);
+
 	
 	if(bind(socketfd, (struct sockaddr*)&serveraddr, sizeof(serveraddr)) < 0){
 		printf("Error on binding");
 		exit(1);
 	}
 	
+	printf("socket= %d\n", socketfd);
 	if(listen(socketfd, 5)==-1) {
-		printf("Error on listening");
-		exit(1);
+		  printf("Error on listening");
+		  exit(1);
 	}
-
-	clientlen = sizeof(clientaddr);
-	if(newsocketfd=accept(socketfd, (struct sockaddr*)&clientaddr, &clientlen)){
-		printf("Error on accepting");
-		exit(1);
-	}
-
-	if(read(newsocketfd, buffer, sizeof(buffer))){
-		printf("Error on binding");
-		exit(1);
-	}
-	printf("%s\n", buffer);
 	
-	if(write(newsocketfd, buffer, sizeof(buffer))){
-		printf("Error on binding");
-		exit(1);
+	while(1){
+	  
+	  clientlen = sizeof(serveraddr);
+	  
+	  if((newsocketfd=accept(socketfd,(struct sockaddr *)&serveraddr,&clientlen))==-1){
+		  printf("Error on accepting");
+		  exit(1);
+	  }
+	  
+	  printf("newsocket= %d\n", newsocketfd);
+	  
+	  memset((void*)&(msgRecieved),(int)'\0',sizeof((msgRecieved)));
+	  
+	  if(n=read(newsocketfd, msgRecieved, sizeof(msgRecieved))==-1){
+		  printf("Error on reading");
+		  exit(1);
+	  }
+	  
+	  printf("nread= %d\n", n);
+	  
+	  printf("%s\n", msgRecieved);
+	  
+	  sprintf(buffer, "Enviaste-me: %s\n", msgRecieved);
+	  
+	  if(write(newsocketfd, buffer, sizeof(buffer))==-1){
+		  printf("Error on writing");
+		  exit(1);
+	  }
 	}
 	
 	close(newsocketfd);
@@ -73,8 +83,3 @@ int main(){
 	
 	return 0;
 }
-
-
-
-
-
