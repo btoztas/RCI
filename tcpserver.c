17a63bebda11000cc96d0cee5458c3695fd82a56
Bruno Gonçalves7 <btoztas@gmail.com>
2016-03-15 20:52:42 +0000

    copiar ficheiros do tcp

--- tcpserver.c
+++ tcpserver.c
@@ -11,62 +11,70 @@
 #define PORT 58000
 
 
-int newtcpserver(struct sockaddr_in *serveraddr, char port[128]){
-  int fd;
-
-  fd=socket(AF_INET, SOCK_STREAM, 0);
-  if(fd==-1){
-    printf("Error opening socket\n");
-    exit(1);
-  }
-
-  memset((void*)&(*serveraddr),(int)'\0',sizeof((*serveraddr)));
-  (*serveraddr).sin_family=AF_INET;
-  (*serveraddr).sin_addr.s_addr=htonl(INADDR_ANY);
-  (*serveraddr).sin_port=htons((u_short)atoi(port));
-
-  return fd;
-}
-
 
 int main(){
 
 	int socketfd, newsocketfd;
-	struct hostent *hostptr;
-	struct sockaddr_in serveraddr, clientaddr;
-	int addrlen, clientlen;
-	char buffer[128];
-	/*struct hostent *h;
-	struct in_addr *a;
-	*/
+	struct sockaddr_in serveraddr;
+	int clientlen;
+	char buffer[128], msgRecieved[128];
+	int n;
 	
-	socketfd=newtcpserver(&serveraddr);
+	
+	
+	memset((void*)&(buffer),(int)'\0',sizeof((buffer)));
+	
+	socketfd=socket(AF_INET, SOCK_STREAM, 0);
+	if(socketfd==-1){
+	  printf("Error opening socket\n");
+	  exit(1);
+	}
+
+	memset((void*)&(serveraddr),(int)'\0',sizeof((serveraddr)));
+	(serveraddr).sin_family=AF_INET;
+	(serveraddr).sin_addr.s_addr=htonl(INADDR_ANY);
+	(serveraddr).sin_port=htons((u_short)PORT);
+
 	
 	if(bind(socketfd, (struct sockaddr*)&serveraddr, sizeof(serveraddr)) < 0){
 		printf("Error on binding");
 		exit(1);
 	}
 	
+	printf("socket= %d\n", socketfd);
 	if(listen(socketfd, 5)==-1) {
-		printf("Error on listening");
-		exit(1);
+		  printf("Error on listening");
+		  exit(1);
 	}
-
-	clientlen = sizeof(clientaddr);
-	if(newsocketfd=accept(socketfd, (struct sockaddr*)&clientaddr, &clientlen)){
-		printf("Error on accepting");
-		exit(1);
-	}
-
-	if(read(newsocketfd, buffer, sizeof(buffer))){
-		printf("Error on binding");
-		exit(1);
-	}
-	printf("%s\n", buffer);
-	sprintf(buffer, "Enviaste-me: %s\n", buffer);
-	if(write(newsocketfd, buffer, sizeof(buffer))){
-		printf("Error on binding");
-		exit(1);
+	
+	while(1){
+	  
+	  clientlen = sizeof(serveraddr);
+	  
+	  if((newsocketfd=accept(socketfd,(struct sockaddr *)&serveraddr,&clientlen))==-1){
+		  printf("Error on accepting");
+		  exit(1);
+	  }
+	  
+	  printf("newsocket= %d\n", newsocketfd);
+	  
+	  memset((void*)&(msgRecieved),(int)'\0',sizeof((msgRecieved)));
+	  
+	  if(n=read(newsocketfd, msgRecieved, sizeof(msgRecieved))==-1){
+		  printf("Error on reading");
+		  exit(1);
+	  }
+	  
+	  printf("nread= %d\n", n);
+	  
+	  printf("%s\n", msgRecieved);
+	  
+	  sprintf(buffer, "Enviaste-me: %s\n", msgRecieved);
+	  
+	  if(write(newsocketfd, buffer, sizeof(buffer))==-1){
+		  printf("Error on writing");
+		  exit(1);
+	  }
 	}
 	
 	close(newsocketfd);
@@ -75,8 +83,3 @@ int main(){
 	
 	return 0;
 }
-
-
-
-
-
